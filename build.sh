d89dcedb0acd3c9672b5c295fe26bd14d5a87e5b
Tobias Anker <tobias.anker@kitsunemimi.moe>
2022-01-31 00:03:39 +0100

    add gitlab-templates

--- build.sh
+++ build.sh
@@ -107,7 +107,7 @@ get_required_private_repo_gitlab "libKitsunemimiHanamiCommon" "v0.1.0" "2ue6RNxk
 get_required_private_repo_gitlab "libKitsunemimiHanamiEndpoints" "v0.1.0" "ysR35grcGsLpFQiXXf1A" 1
 get_required_private_repo_gitlab "libKitsunemimiHanamiDatabase" "master" "UjDbSSJzoU3uK4pZynmD" 8
 get_required_private_repo_gitlab "libKitsunemimiHanamiPolicies" "v0.1.0" "MbPGESTo6zBzcXZW7_T8" 1
-get_required_private_repo_gitlab "libKitsunemimiHanamiMessaging" "v0.2.0" "vkEae-QF8jvt9W2xz3LR" 8
+get_required_private_repo_gitlab "libKitsunemimiHanamiMessaging" "master" "vkEae-QF8jvt9W2xz3LR" 8
 get_required_private_repo_gitlab "libKitsunemimiHanamiPredefinitions" "v0.1.0" "8_D9Z_gRjC7TmFQi-tsD" 8
 echo ""
 echo "###########################################################################################################"
